9b41a44131beea0d61f8c64aff48435447d79822
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2023-09-19 02:20:14 +0600

    upper calculation added

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-document.getElementById('brg-plus').addEventListener('click', function(){
-    const quantityField = document.getElementById('burg-value');
+// common funtion================
+function handlerValue(value,id) {
+    const quantityField = document.getElementById(`${id}`);
     const quantityValue = quantityField.value;
-    const quantityInt= parseInt(quantityValue);
-    console.log(quantityValue)
-    const newValue= quantityInt+1;
-    quantityField.value=newValue;
+    const quantityInt = parseInt(quantityValue);
+    let newValue;
+    if (value==true) {
+         newValue = quantityInt + 1;
+    }
+    else {
+         newValue = quantityInt - 1;
+    }
+    if(newValue<0){
+        return 0;
+    }
+    quantityField.value = newValue;
+    return newValue;
+}
 
+function updateTotal(itemNumber,price,id){
+    const total = itemNumber * price;
+    document.getElementById(`${id}`).innerHTML = total;
+}
+
+///burger==================
+
+document.getElementById('brg-plus').addEventListener('click', function () {
+    const itemNumber = handlerValue(true, 'burg-value');
+    updateTotal(itemNumber, 15,'brg_amount');
+})
+
+document.getElementById('brg-minus').addEventListener('click', function () {
+   const item= handlerValue(false,'burg-value');
+    updateTotal(item,15, 'brg_amount');
+})
+// pizza=============
+
+document.getElementById('pizza-plus').addEventListener('click', function () {
+    const itemNumber = handlerValue(true, 'pizza-value');
+    updateTotal(itemNumber, 27, 'pizza_amount');
+})
+
+document.getElementById('pizza-minus').addEventListener('click', function () {
+    const item = handlerValue(false,'pizza-value');
+    updateTotal(item, 27, 'pizza_amount');
 })
